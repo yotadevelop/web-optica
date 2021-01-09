4ae0cdf627ed3b162fcf7977b7ce6c34230a3109
yota <jonathanvalenzuelaortega@gmail.com>
2021-01-08 21:58:45 -0300

    modificación al modelo conexión

--- models/Conexion.php
+++ models/Conexion.php
@@ -4,9 +4,9 @@ namespace models;
 
 class Conexion{
 
-    public static $user ="root";
-    public static $pass ="";
-    public static $URL  ="mysql:host=localhost;dbname=optica_2020";
+    public static $user ="ugd3vzhgoggpqvn2";
+    public static $pass ="DYiUKbbW5n8n3nhN0l6E";
+    public static $URL  ="mysql:host=brttyhx0tdhbgrmwhbm8-mysql.services.clever-cloud.com;dbname=brttyhx0tdhbgrmwhbm8";
 
     public static function conector(){
         try{
